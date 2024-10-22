0de34f60ce1c05f5bd6a8d7dd9a9545123f503c7
Dhanshri <dhanshri20@navgurukul.org>
2024-10-22 21:49:23 +0530

    connecting mongodb with node

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -6,12 +6,15 @@ require('dotenv').config();
 
 async function dbConnect() {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.DB_URL, {
             useNewUrlParser: true, // to avoid deprecation warnings
             useUnifiedTopology: true,  // to avoid deprecation warnings
         });
-        console.log('Connected to the mongoDb database');
+        console.log('Connected to the mongoDb database atlas successfully');
     } catch (error) {
         console.log('Error connecting to the database', error);
     }
 }
+
+
+module.exports = dbConnect;
--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = require('./app');
 
 
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT ?? 3000);
 
 
 const server = http.createServer(app);
 
-server.maxConnections(`listening`, () =>{
+server.on(`listening`, () =>{
     console.log(`Listening on port` +(process.env.PORT ?? 3000));
 })
 
